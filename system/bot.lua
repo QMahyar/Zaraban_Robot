3ca3a8aae9d094537b4bdd6cb4ad20435df7748b
Mahyartanha <mahyar.drama1313@gmail.com>
2017-05-11 01:42:16 +0430

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -232,18 +232,14 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {211492300,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
 This is an original bot and based on (AdvanSource).
 Copyright all right reserved and you must respect all laws.
 
-Source: https://github.com/janlou/AdvanSource
-Channel: @AdvanTeam
-Messenger: @Advanbot
-Creator: @janlou
-Site: http://StoreVps.net
+Source: https://github.com/Mahyartanha/Zaraban_Robot
 Version: [4.1]
 ]],
     --Start rate:
